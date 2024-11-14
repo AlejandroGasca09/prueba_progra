ba6bb9c3660f31b24af6f537d4a5fd158b69c973
AlejandroGasca09 <119777270+AlejandroGasca09@users.noreply.github.com>
2024-11-13 23:05:29 -0600

    Add files via upload

---
+++ prueba_bd.sql
@@ -0,0 +1,5 @@
+SELECT * FROM proyecto_progra;
+
+INSERT INTO `prueba_proyecto`.`new_table` (`nombre`, `correo`) VALUES ('Alejandro', 'ale@gmail.com');
+INSERT INTO `prueba_proyecto`.`new_table` (`nombre`, `correo`) VALUES ('lucaas', 'luca@gmail.com');
+INSERT INTO `prueba_proyecto`.`new_table` (`nombre`, `correo`) VALUES ('sofia', 'sofia@gmail.com');
